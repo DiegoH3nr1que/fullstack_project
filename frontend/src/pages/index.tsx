766b59d5c9b99234eda954bc1848ceb2c75bc43c
GiovanniM3n0n <giovannimenon51@gmail.com>
2024-06-12 00:39:28 -0300

    refactor(frontend): corrections inital page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,33 +4,42 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Carousel } from 'react-responsive-carousel';
 import axios from 'axios';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+interface Game {
+  name: string;
+  background_image: string;
+  released: string;
+  rating: number;
+}
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [highlightedImages, setHighlightedImages] = useState<string[]>([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     axios.get('http://localhost:8000/games/')
       .then(response => {
+        console.log(response.data);
         if (Array.isArray(response.data)) {
-          setHighlightedImages(response.data);
+          setGames(response.data);
         } else {
           console.error('A resposta da API não é uma matriz:', response.data);
         }
       })
       .catch(error => console.error('Error fetching games:', error));
   }, []);
-  
+
   const handleNext = () => {
-    setCurrentPage((prevPage) => (prevPage + 1) % Math.ceil(highlightedImages.length / 3));
+    setCurrentPage((prevPage) => (prevPage + 1) % Math.ceil(games.length / 3));
   };
 
   const handlePrev = () => {
-    setCurrentPage((prevPage) => (prevPage - 1 + Math.ceil(highlightedImages.length / 3)) % Math.ceil(highlightedImages.length / 3));
+    setCurrentPage((prevPage) => (prevPage - 1 + Math.ceil(games.length / 3)) % Math.ceil(games.length / 3));
   };
 
-  const currentImages = highlightedImages.slice(currentPage * 3, (currentPage + 1) * 3);
+  const currentGames = games.slice(currentPage * 3, (currentPage + 1) * 3);
 
   return (
     <>
@@ -71,9 +80,14 @@ const Home = () => {
             interval={3000}
             transitionTime={600}
           >
-            {highlightedImages.map((image, index) => (
+            {games.map((game, index) => (
               <div key={index} className="hover:scale-105 transition-transform duration-300">
-                <Image src={image} alt={`Highlighted Game ${index + 1}`} width={500} height={300} />
+                <Image src={game.background_image} alt={game.name} layout="responsive" width={500} height={300} />
+                <div className="carousel-caption">
+                  <h3>{game.name}</h3>
+                  <p>Released: {game.released}</p>
+                  <p>Rating: {game.rating}</p>
+                </div>
               </div>
             ))}
           </Carousel>
@@ -82,9 +96,12 @@ const Home = () => {
           <div className="flex items-center">
             <button onClick={handlePrev} className="bg-gray-800 p-2 rounded">Prev</button>
             <div className="grid grid-cols-3 gap-4 mx-4">
-              {currentImages.map((image, index) => (
+              {currentGames.map((game, index) => (
                 <div key={index} className="bg-gray-800 p-4 rounded hover:scale-105 transition-transform duration-300">
-                  <Image src={image} alt={`Highlighted Game ${index + 1}`} width={200} height={300} />
+                  <Image src={game.background_image} alt={game.name} layout="responsive" width={200} height={300} />
+                  <h3 className="mt-2">{game.name}</h3>
+                  <p>Released: {game.released}</p>
+                  <p>Rating: {game.rating}</p>
                 </div>
               ))}
             </div>
@@ -95,10 +112,12 @@ const Home = () => {
         <section className="container mx-auto py-10">
           <h2 className="text-2xl font-bold mt-6">Lançamentos</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
-            {highlightedImages.slice(0, 3).map((image, index) => (
+            {games.slice(0, 3).map((game, index) => (
               <div key={index} className="bg-gray-800 p-4 rounded">
-                <Image src={image} alt={`Game ${index + 1}`} width={200} height={300} />
-                <h3 className="mt-2">{`Game ${index + 1}`}</h3>
+                <Image src={game.background_image} alt={game.name} layout="responsive" width={200} height={300} />
+                <h3 className="mt-2">{game.name}</h3>
+                <p>Released: {game.released}</p>
+                <p>Rating: {game.rating}</p>
               </div>
             ))}
           </div>
